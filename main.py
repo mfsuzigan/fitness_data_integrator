e17327e71dacb5e6f358a0be3d2e58d3976af5fc
mfsuzigan <mfsuzigan@gmail.com>
2024-06-02 00:21:55 -0300

    (WIP) experimenting with formulas

--- main.py
+++ main.py
@@ -6,9 +6,8 @@ SPREADSHEET_ID = "1ETe9O2qX36aRx6243qzYNMNChQKZmxvWdRMt7htPLaM"
 def main():
     gc = gspread.service_account(filename="creds.json")
     sheet = gc.open_by_key(SPREADSHEET_ID).sheet1
-    row = ["1", "2", "3", "4"]
-    sheet.append_row(row)
-
+    row = ["=average(2, 2, 1)", "2", "3", "4"]
+    sheet.append_row(row, value_input_option="USER_ENTERED")
 
 if __name__ == "__main__":
     main()
