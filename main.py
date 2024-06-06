5093fab6408cc89f5d603e5de6a55920c58b0129
mfsuzigan <mfsuzigan@gmail.com>
2024-06-06 02:33:04 -0300

    serving index.html, 1st functional version

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import gspread
 from gspread.utils import ValueInputOption
 
@@ -6,11 +6,17 @@ SPREADSHEET_ID = "1ETe9O2qX36aRx6243qzYNMNChQKZmxvWdRMt7htPLaM"
 app = Flask(__name__)
 
 
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+
 @app.route('/measures', methods=['POST'])
 def measures():
     measures_data = request.get_json()
     weight_measures = measures_data.get('weight')
-    fat_measures = measures_data.get('fat%')
+    fat_measures = measures_data.get('fat_perc')
+    send_data_to_spreadsheet(weight_measures)
     return jsonify({'message': 'Measures received successfully'}), 200
 
 
@@ -19,10 +25,10 @@ def preflight():
     return jsonify()
 
 
-def main():
+def send_data_to_spreadsheet(weight_measures):
     gc = gspread.service_account(filename="resources/creds.json")
     sheet = gc.open_by_key(SPREADSHEET_ID).sheet1
-    row = ["=average(2, 2, 1)", "2", "3", "4"]
+    row = [f"=average{tuple(weight_measures)}", "test", "test", "test"]
     sheet.append_row(row, value_input_option=ValueInputOption.user_entered)
 
 
@@ -36,4 +42,4 @@ def add_cors_headers(response):
 app.after_request(add_cors_headers)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8888, host="0.0.0.0")
