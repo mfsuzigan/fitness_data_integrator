de3b6a2228b2146f4b93ba2ea93217fcd0518211
mfsuzigan <mfsuzigan@gmail.com>
2024-06-07 01:40:40 -0300

    removing obsolete CORS treatment

--- main.py
+++ main.py
@@ -14,15 +14,14 @@ def index():
 @app.route('/measures', methods=['POST'])
 def measures():
     measures_data = request.get_json()
+    app.logger.info(measures_data)
+
     weight_measures = measures_data.get('weight')
     fat_measures = measures_data.get('fat_perc')
+
     send_data_to_spreadsheet(weight_measures)
+
     return jsonify({'message': 'Measures received successfully'}), 200
-
-
-@app.route('/', methods=['OPTIONS'])
-def preflight():
-    return jsonify()
 
 
 def send_data_to_spreadsheet(weight_measures):
@@ -31,15 +30,6 @@ def send_data_to_spreadsheet(weight_measures):
     row = [f"=average{tuple(weight_measures)}", "test", "test", "test"]
     sheet.append_row(row, value_input_option=ValueInputOption.user_entered)
 
-
-def add_cors_headers(response):
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS'
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-    return response
-
-
-app.after_request(add_cors_headers)
 
 if __name__ == "__main__":
     app.run(debug=True, port=8888, host="0.0.0.0")
