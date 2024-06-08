d2328379d4ce63295f5b41ab65b7a190f12dd495
mfsuzigan <mfsuzigan@gmail.com>
2024-06-08 01:54:39 -0300

    refactoring into layers

---
+++ controller.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify, render_template
+
+from service import save_data
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+
+@app.route('/measurement', methods=['POST'])
+def save_measurement():
+    raw_data = request.get_json()
+    app.logger.info(raw_data)
+    save_data(raw_data)
+
+    return jsonify({'message': 'Measures received successfully'})
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8888, host="0.0.0.0")
