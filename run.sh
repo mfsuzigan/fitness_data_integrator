092152c14fd983dccfab6a599e88cc16078e89fd
mfsuzigan <mfsuzigan@gmail.com>
2024-08-04 12:33:40 -0300

    container startup script documentation

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
+# For testing execution, pass "test" as command line parameter:
+#     ./run.sh test
+
 docker rm -f fitness_data_integrator 2> /dev/null
 environment="${1:-production}"
 docker run -it -e FLASK_ENV=$environment -p 8888:8888 --restart always --name fitness_data_integrator fitness_data_integrator:latest
