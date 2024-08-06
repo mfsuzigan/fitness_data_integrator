9fa4de1d13a9292ba2af8d7c78670b003bf81a4c
mfsuzigan <mfsuzigan@gmail.com>
2024-08-06 00:31:23 -0300

    refactoring

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import json
 import os
-
 from flask import Flask, request, jsonify, render_template
-
 from service import save_to_spreadsheet, save_to_requests_log
 
 app = Flask(__name__)
 config_type = os.getenv('FLASK_ENV', 'production')
 
-if config_type == 'test':
+if config_type.lower() == 'test':
     app.config.from_object('config_test.TestConfig')
     app.logger.warning("⚠️  Running TEST environment️")
 
 else:
     app.config.from_object('config.Config')
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8888, host="0.0.0.0")
 
 
 @app.route("/", methods=["GET"])
@@ -29,7 +30,3 @@ def save_measurement():
     save_to_spreadsheet(input_data, app.config['SPREADSHEET_ID'])
 
     return jsonify({'message': 'Measurements received successfully'})
-
-
-if __name__ == "__main__":
-    app.run(debug=True, port=8888, host="0.0.0.0")
